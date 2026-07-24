c759e63b567ecb7151d62630182102f368d9949b
kashw <kashwanth206@gmail.com>
2026-07-24 11:15:58 +0530

    hi

---
+++ src/day1/problem.java
@@ -0,0 +1,19 @@
+package day1;
+
+class Problem1 {
+    public int addDigits(int num) {
+        while (num >= 10) {
+            int sum = 0;
+
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            num = sum;
+        }
+
+        return num;
+    }
+}public class problem {
+}
